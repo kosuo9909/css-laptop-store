b7c29ab938829a7386ab22c43ff06326aea837d4
Kostadin Stanev <stanebvns@gmail.com>
2023-08-16 09:32:39 +0300

    completed laptopstore v1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,18 @@
 import './Navbar.css';
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <nav className='main-nav'>
+      <ul className='ul-wrapper'>
+        <li>Apple</li>
+        <li>Toshiba</li>
+        <li>Lenovo</li>
+        <li>Dell</li>
+        <li>Asus</li>
+        <li>Hacer</li>
+        <li>HP</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/components/RightSlidersPanel.jsx
+++ src/components/RightSlidersPanel.jsx
@@ -1,16 +1,70 @@
+import { useState } from 'react';
 import './RightSlidersPanel.css';
 const RightSlidersPanel = () => {
+  const [minPrice, setMinPrice] = useState(500);
+  const [maxPrice, setMaxPrice] = useState(1500);
+  const [minRam, setMinRam] = useState(4);
+  const [maxRam, setMaxRam] = useState(16);
   return (
-    <div class='slidecontainer'>
-      <input
-        type='range'
-        min='1'
-        max='100'
-        value='50'
-        class='slider'
-        id='myRange'
-      ></input>
-    </div>
+    <aside className='right-aside'>
+      <div className='slidecontainer'>
+        <label htmlFor='min-price-range' className='text18px'>
+          Min. Price: {minPrice} USD
+        </label>
+
+        <input
+          type='range'
+          min='0'
+          max='1000'
+          value={minPrice}
+          onChange={(e) => setMinPrice(e.target.value)}
+          className='slider'
+          id='min-price-range'
+        ></input>
+      </div>
+      <div className='slidecontainer'>
+        <label htmlFor='max-price-range' className='text18px'>
+          Max. Price: {maxPrice} USD
+        </label>
+        <input
+          type='range'
+          min='0'
+          max='3000'
+          value={maxPrice}
+          onChange={(e) => setMaxPrice(e.target.value)}
+          className='slider'
+          id='max-price-range'
+        ></input>
+      </div>
+      <div className='slidecontainer'>
+        <label htmlFor='min-ram-range' className='text18px'>
+          Min. RAM: {minRam} GB
+        </label>
+        <input
+          type='range'
+          min='0'
+          max='8'
+          value={minRam}
+          onChange={(e) => setMinRam(e.target.value)}
+          className='slider'
+          id='min-ram-range'
+        ></input>
+      </div>
+      <div className='slidecontainer'>
+        <label htmlFor='max-ram-range' className='text18px'>
+          Max. RAM: {maxRam} GB
+        </label>
+        <input
+          type='range'
+          min='0'
+          max='32'
+          value={maxRam}
+          onChange={(e) => setMaxRam(e.target.value)}
+          className='slider'
+          id='max-ram-range'
+        ></input>
+      </div>
+    </aside>
   );
 };
 
