6edf7536b7831d7c57bb83e961f35ba605a5d334
Kostadin Stanev <stanebvns@gmail.com>
2023-08-21 18:41:51 +0300

    replace an absolute el. with fieldset+legend

--- src/components/MacBookSingleItem.jsx
+++ src/components/MacBookSingleItem.jsx
@@ -1,17 +1,19 @@
 import './MacBookSingleItem.scss';
 const MacBookSingleItem = ({ title, img, price, storage, ram, processor }) => {
   return (
-    <section className="single-macbook-render">
-      <h6 className="macbook-title">{title}</h6>
-      <div className="image-price-wrapper">
-        <img src={img} alt="macbook air" className="macbook-img"></img>
-        <span>{price} USD</span>
-      </div>
-      <div className="laptop-details-wrapper">
-        <span>{storage}</span>
-        <span>{ram}</span>
-        <span>{processor}</span>
-      </div>
+    <section>
+      <fieldset className='single-macbook-render'>
+        <legend>{title}</legend>
+        <div className='image-price-wrapper'>
+          <img src={img} alt='macbook air' className='macbook-img'></img>
+          <span>{price} USD</span>
+        </div>
+        <div className='laptop-details-wrapper'>
+          <span>{storage}</span>
+          <span>{ram}</span>
+          <span>{processor}</span>
+        </div>
+      </fieldset>
     </section>
   );
 };
